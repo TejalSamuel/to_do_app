2fe18040c120ab63c62031aa6419c312bdb60234
Tejal Samuel <tejalsamuel02@gmail.com>
2024-07-26 20:20:12 +0530

    changes made

--- to_do_project/settings.py
+++ to_do_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,6 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+APPEND_SLASH = False
 
 # Application definition
 
@@ -66,7 +68,7 @@ ROOT_URLCONF = 'to_do_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'myapp','templates', 'myapp')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,7 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIR = [ os.path.join(BASE_DIR, 'to_do_project','static')]
+os.makedirs(os.path.join(BASE_DIR, 'to_do_project','assets'), exist_ok=True)
+STATIC_ROOT = os.path.join(BASE_DIR, 'to_do_project','assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- to_do_project/urls.py
+++ to_do_project/urls.py
@@ -21,6 +21,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
- path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
 ]
